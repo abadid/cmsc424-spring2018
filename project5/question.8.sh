54a8c2733634e541b8e53370c583b52a07b5659f
Daniel Abadi <abadid@gmail.com>
2018-04-03 14:46:11 -0400

    fix q7 and q8 and add q9

--- project5/question.8.sh
+++ project5/question.8.sh
@@ -3,17 +3,7 @@
 TIMEFORMAT='%3R'
 
 sudo -u vagrant psql --quiet -d app -f drop-indexes.sql
-
-echo -e "\n"
-echo "Creating UNIQUE index on username..."
-sudo -u vagrant -H -- psql --quiet -d app -c \
-    "CREATE UNIQUE INDEX uniq_username ON users (username);"
-
-echo "Creating index on date_of_birth..."
-sudo -u vagrant -H -- psql --quiet -d app -c \
-    "CREATE INDEX users_date_of_birth ON users (date_of_birth);"
-
-echo "Two indexes created."
+sudo -u vagrant psql --quiet -d app -f q8.sql
 
 echo -e "\n"
 echo "Question 8"
@@ -21,15 +11,11 @@ echo "-------------------------------------"
 echo "Executing Q8.1..."
 sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' 
 time sudo -u vagrant -H -- psql --quiet -d app -c \
-    "UPDATE users
-     SET username = 'kilobyte1'
-     WHERE id = '228521';" 
+    "update q8_users1 set id = (extract(year from date_of_birth)::char(4)||id::varchar(10))::int;" 
 
 
 echo -e "\n\n-------------------------------------"
 echo "Executing Q8.2..."
 sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches' 
 time sudo -u vagrant -H -- psql --quiet  -d app -c \
-    "UPDATE users
-     SET date_of_birth = date_of_birth + interval '1 year'
-     WHERE id = '89976';" 
+    "update q8_users2 set id = (extract(year from date_of_birth)::char(4)||id::varchar(10))::int;" 
