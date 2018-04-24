ca3ed0506dd76603422c151f6fe1ab582959645d
bsou <bsouvik@cs.umd.edu>
2018-04-23 23:09:12 -0400

    ...

--- project6/queryproc/JoinOperators.java
+++ project6/queryproc/JoinOperators.java
@@ -3,12 +3,12 @@ package queryproc;
 public class JoinOperators {
 
     public static Relation<TupleType3> MergeJoinOnIntegerAttributes(Relation<TupleType1> leftRelation, Relation<TupleType2> rightRelation,
-                                                     int lrJoinAttributeIndex, int rrJoinAttributeIndex) {
+            int lrJoinAttributeIndex, int rrJoinAttributeIndex) {
 
         Relation<TupleType3> resultRelation = new Relation<TupleType3>();
 
         /*
-        
+
         // You may use the following code snippet in your code. Remember to follow the same order of attributes
         // in the ResultRelation as shown below
 
@@ -21,8 +21,25 @@ public class JoinOperators {
         // this is the order that you must follow while inserting attributes into the result relation
         resultRelation.insert(new TupleType3(lrTuple.getAllAttributes(), rrTuple.getAllAttributes()));
 
-        */
+         */
 
+        int left = 0, right = 0;
+        while (left < leftRelation.getSize() && right < rightRelation.getSize()) {
+            TupleType1 lrTuple = leftRelation.getTuple(left);
+            TupleType2 rrTuple = rightRelation.getTuple(right);
+
+            int left_key = (Integer) lrTuple.getAttribute(lrJoinAttributeIndex);
+            int right_key = (Integer) rrTuple.getAttribute(rrJoinAttributeIndex);
+
+            if (left_key == right_key) {
+                resultRelation.insert(new TupleType3(lrTuple.getAllAttributes(), rrTuple.getAllAttributes()));
+                right++;
+            } else if (left_key < right_key) {      
+                left++;
+            } else {
+                right++;
+            }
+        }
         return resultRelation;
     }
 }
