65eb6a19aa31a6f20a50a51a3344283abd08e2a2
Brian Mitchell <mitchell.brian82@gmail.com>
2018-04-27 16:27:58 -0400

    Added skelton for python

---
+++ project7/python/tasks.py
@@ -0,0 +1,27 @@
+import json
+import re
+from pyspark import SparkContext
+
+def task1(playRDD):
+    return playRDD
+
+def task2_flatmap(x):
+    return []
+
+def task3(nobelRDD):
+    return nobelRDD
+
+def task4(logsRDD, l):
+    return logsRDD
+
+def task5(bipartiteGraphRDD):
+    return bipartiteGraphRDD
+
+def task6(logsRDD, day1, day2):
+    return logsRDD
+
+def task7(nobelRDD):
+    return nobelRDD
+
+def task8(flewon, n, m):
+    return flewon
