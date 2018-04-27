6c3adfd52e40a54bfb05e3551a13ee0d6c7e947a
Kevin Wittmer <wittmerkevinr@gmail.com>
2018-04-27 15:10:52 -0400

    Added skeleton class for fragment-and-replicate join

---
+++ project7/java/src/main/java/FragmentAndReplicateJoin.java
@@ -0,0 +1,14 @@
+import org.apache.spark.api.java.*;
+import scala.*;
+
+public class FragmentAndReplicateJoin {
+
+    /*
+     * Joins leftRelation and rightRelation using the fragment-and-replicate join algorithm. Your implementation should partition
+     * leftRelation into n partitions and rightRelation into m partitions.
+     */
+    private static JavaRDD<Tuple4<String, Double, String, Double>> fragmentAndReplicateJoin(JavaRDD<Tuple2<String, Double>> leftRelation, int n, JavaRDD<Tuple2<String, Double>> rightRelation, int m) {
+        return null;
+    }
+
+}
